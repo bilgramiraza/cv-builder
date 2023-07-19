e975fe5c565b4c0abe06467d26e119bcd475018d
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-19 09:00:21 +0530

    Added more Fields for the InputGroup

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
-function InputGroup({inputName, inputType, inputLabel}) {
+function InputGroup({inputName, inputType, inputLabel, inputValue, handleChange}) {
   return (
     <div>
       <label htmlFor={inputName}>{inputLabel}</label>
-      <input type={inputType} name={inputName} />
+      <input type={inputType} name={inputName} onChange={handleChange} value={inputValue}/>
     </div>
   );
 }
@@ -13,6 +13,8 @@ InputGroup.propTypes = {
   inputName: PropTypes.string.isRequired,
   inputType: PropTypes.string.isRequired,
   inputLabel: PropTypes.string.isRequired,
+  inputValue: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default InputGroup;
