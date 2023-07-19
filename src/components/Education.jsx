98f07093aedb0ed8cd628a3b1fce94e5d8f25d32
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-19 10:23:53 +0530

    Setting up Single Entry Form for Education

---
+++ src/components/Education.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import PropTypes from 'prop-types';
+import InputGroup from "./InputGroup";
+
+function Education({getEducation}) {
+  const [jobTitle, setJobTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  
+  const handleChange = (e) =>{
+    switch(e.target.name){
+      case 'jobTitle':
+        setJobTitle(e.target.value);
+        break;
+      case 'desc':
+        setDesc(e.target.value);
+        break;
+      default:
+        throw new Error('How did we get Here');
+    }
+  };
+
+  const handleSubmit= (e) =>{
+    e.preventDefault();
+    const data = {
+      jobTitle,
+      desc,
+    };
+    getEducation(data);
+  };
+
+  return (
+    <div>
+      <h3>Education Details</h3>
+      <form onSubmit={handleSubmit}>
+        <InputGroup inputType="text" inputName="jobTitle" inputLabel="Job Title" inputValue={jobTitle} handleChange={handleChange}/>
+        <InputGroup inputType="desc" inputName="desc" inputLabel="Description" inputValue={desc} handleChange={handleChange}/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Education;
+
+
+Education.propTypes = {
+  getEducation: PropTypes.func.isRequired,
+};
