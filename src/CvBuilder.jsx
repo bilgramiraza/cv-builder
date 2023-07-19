ae779468d6e40788e5fbd696516be26326f9ef0d
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-19 09:03:54 +0530

    Implemented Personal Fields Component

--- src/CvBuilder.jsx
+++ src/CvBuilder.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import InputGroup from "./components/InputGroup";
+import Personal from "./components/Personal";
 
 function CvBuilder() {
+  const [cv, setCv] = useState({
+    personal:null,
+    education:[],
+    experience:[],
+  });
+  const handleSubmit = (category, value) =>{
+    switch(category){
+      case 'personal':
+        setCv({
+          ...cv,
+          personal:value,
+        });
+        break;
+      case 'education':
+        break;
+      case 'experience':
+        break;
+      default:
+        throw new Error('How did we get Here');
+    }
+  }
   return (
     <main>
-      <form>
-        <h3>Personal Details</h3>
-        <InputGroup inputType="text" inputName="firstName" inputLabel="First Name" />
-        <InputGroup inputType="text" inputName="firstName" inputLabel="First Name" />
-        <InputGroup inputType="email" inputName="email" inputLabel="Email" />
-        <h3>Education</h3>
-        <InputGroup inputType="text" inputName="jobTitle" inputLabel="Job Title" />
-        <InputGroup inputType="number" inputName="jobExp" inputLabel="Job Experience(YOE)" />
-        <h3>Experience</h3>
-        <InputGroup inputType="text" inputName="eduTitle" inputLabel="Education Title" />
-        <InputGroup inputType="number" inputName="eduExp" inputLabel="Education Experience(YOE)" />
-      </form>
+      <Personal getPersonal={(value)=>handleSubmit('personal',value)}/>
+      <h3>Education</h3>
+      <InputGroup inputType="text" inputName="jobTitle" inputLabel="Job Title" />
+      <InputGroup inputType="number" inputName="jobExp" inputLabel="Job Experience(YOE)" />
+      <h3>Experience</h3>
+      <InputGroup inputType="text" inputName="eduTitle" inputLabel="Education Title" />
+      <InputGroup inputType="number" inputName="eduExp" inputLabel="Education Experience(YOE)" />
     </main>
   );
 }
