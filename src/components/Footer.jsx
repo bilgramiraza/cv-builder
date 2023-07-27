a4e6bb5c39302f80d849065446acb89c5c316a03
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-27 23:02:31 +0530

    Cleaned Up UI of Footer Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 function Footer() {
   return (
-    <footer>
-      <p>Built by Raza Hassan</p>
+    <footer className="mt-5 dark">
+      <hr className="border-gray-200 dark:border-gray-700 sm:mx-auto sm:mt-1"/>
+      <div className="py-4 bg-white md:px-6 dark:bg-gray-800">
+        <span className="block text-xl text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/bilgramiraza" target="_blank" rel="noreferrer" className="hover:underline">Built by S.M.Raza Hassan</a>. All Rights Reserved.</span>
+      </div>
     </footer>
   );
 }
