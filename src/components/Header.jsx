0808d01d544fa7beda9d6a9de4e0c023e33b51ef
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-27 22:11:52 +0530

    Cleaned Up UI of Header Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 function Header() {
   return (
-    <header>
-      <h1>CV Builder</h1>
-    </header>
+    <div className="dark">
+      <header className="bg-gray-300 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-center mx-auto max-w-screen-xl">
+          <span className=" self-center text-3xl text-black font-bold whitespace-nowrap dark:text-white">
+            CV Builder
+          </span>
+        </div>
+      </header>
+      <hr className="border-gray-900 sm:mx-auto dark:border-gray-500"/>
+    </div>
   );
 }
 
