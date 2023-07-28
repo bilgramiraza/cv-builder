5c291af3eed9a3e622479f11ccf97fe424e8dfda
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-28 10:44:30 +0530

    Improving Input UI using TailwindCSS

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,6 +1,12 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function InputGroup({inputName, inputType, inputLabel, inputValue, handleChange, disabled}) {
+  const [isValid, setIsValid] = useState(true);
+  const handleInputChange = (e) => {
+    handleChange(e);
+    setIsValid(e.target.validity.valid);
+  };
   let inputGroup;
   switch(inputType){
     case 'desc':
@@ -8,9 +14,10 @@ function InputGroup({inputName, inputType, inputLabel, inputValue, handleChange,
         <textarea 
           id={inputName} 
           name={inputName} 
-          onChange={handleChange} 
+          onChange={handleInputChange} 
           value={inputValue} 
           disabled={disabled}
+          className={`block w-full px-1 py-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border text-black ${isValid ?'border-gray-300':'border-red-500'} rounded-r-md`}
         />
       );
       break;
@@ -20,17 +27,23 @@ function InputGroup({inputName, inputType, inputLabel, inputValue, handleChange,
           id={inputName} 
           type={inputType} 
           name={inputName} 
-          onChange={handleChange} 
+          onChange={handleInputChange} 
           value={inputValue}
           disabled={disabled}
+          className={`block w-3/4 px-1 py-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border text-black ${isValid ?'border-gray-300':'border-red-500'} rounded-r-md`}
         />
       );
       break;
   }
   return (
-    <div>
-      <label htmlFor={inputName}>{inputLabel}</label>
-      {inputGroup}
+    <div className='flex items-start'>
+      <label htmlFor={inputName} className='block mt-3 text-start text-lg sm:text-sm font-medium text-gray-300'>
+        {inputLabel}
+      </label>
+      <div className='flex items-center mt-1'>
+        {inputGroup}      
+    </div>
+    {!isValid && <div className="text-red-600 text-sm mt-1">Invalid Input</div>}
     </div>
   );
 }
