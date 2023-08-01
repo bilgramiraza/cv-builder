e49ba7e1ad5f01b471add11cc1b60840ac947f58
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-02 00:32:35 +0530

    Improved UI for Input Component

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -17,7 +17,8 @@ function InputGroup({inputName, inputType, inputLabel, inputValue, handleChange,
           onChange={handleInputChange} 
           value={inputValue} 
           disabled={disabled}
-          className={`block w-full px-1 py-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border text-black ${isValid ?'border-gray-300':'border-red-500'} rounded-r-md`}
+          rows={4}
+          className={`w-11/12 px-1 py-1 text-sm lg:text-base text-black rounded-r-md bg-gray-300 text-black border ${isValid ?'border-gray-300':'border-red-500'} focus:border-indigo-500 focus:ring-indigo-500 dark:bg-gray-800 dark:text-white`}
         />
       );
       break;
@@ -30,21 +31,21 @@ function InputGroup({inputName, inputType, inputLabel, inputValue, handleChange,
           onChange={handleInputChange} 
           value={inputValue}
           disabled={disabled}
-          className={`block w-3/4 px-1 py-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border text-black ${isValid ?'border-gray-300':'border-red-500'} rounded-r-md`}
+          className={`w-11/12 px-1 py-1 text-sm lg:text-base text-black rounded-r-md bg-gray-300 text-black border ${isValid ?'border-gray-300':'border-red-500'} focus:border-indigo-500 focus:ring-indigo-500 dark:bg-gray-800 dark:text-white`}
         />
       );
       break;
   }
   return (
-    <div className='flex items-start'>
-      <label htmlFor={inputName} className='block mt-3 text-start text-lg sm:text-sm font-medium text-gray-300'>
+    <>
+      <label htmlFor={inputName} className='flex w-auto mt-3 text-start text-xs 2xl:text-lg text-black dark:text-white'>
         {inputLabel}
       </label>
-      <div className='flex items-center mt-1'>
+      <div className='flex w-auto mt-1 '>
         {inputGroup}      
-    </div>
-    {!isValid && <div className="text-red-600 text-sm mt-1">Invalid Input</div>}
-    </div>
+      </div>
+      {!isValid && <div className="text-red-600 text-sm mt-1">Invalid Input</div>}
+    </>
   );
 }
 
