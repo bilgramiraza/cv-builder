586f4be91509f6d8703b1969e933c483d9965341
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-04 02:23:27 +0530

    Fixed Inconsistent CSS between Header and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <footer className="mt-5 dark">
-      <hr className="border-gray-200 dark:border-gray-700 sm:mx-auto sm:mt-1"/>
-      <div className="py-4 bg-white md:px-6 dark:bg-gray-800">
-        <span className="block text-xl text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/bilgramiraza" target="_blank" rel="noreferrer" className="hover:underline">Built by S.M.Raza Hassan</a>. All Rights Reserved.</span>
+    <footer className="dark">
+      <hr className="border-gray-900 dark:border-gray-500"/>
+      <div className="flex justify-center bg-gray-300 border-gray-200 py-2.5 dark:bg-gray-800">
+        <span className="text-gray-500 text-center text-xl dark:text-gray-400">© 2023 <a href="https://github.com/bilgramiraza" target="_blank" rel="noreferrer" className="hover:underline">Built by S.M.Raza Hassan</a>. All Rights Reserved.</span>
       </div>
     </footer>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
 function Header() {
   return (
-    <div className="dark">
-      <header className="bg-gray-300 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div className="flex flex-wrap justify-center mx-auto max-w-screen-xl">
-          <span className=" self-center text-3xl text-black font-bold whitespace-nowrap dark:text-white">
+    <header className="dark">
+        <div className="flex justify-center bg-gray-300 border-gray-200 py-2.5 dark:bg-gray-800">
+          <span className="text-3xl text-black font-bold dark:text-white">
             CV Builder
           </span>
         </div>
-      </header>
-      <hr className="border-gray-900 sm:mx-auto dark:border-gray-500"/>
-    </div>
+      <hr className="border-gray-900 dark:border-gray-500"/>
+    </header>
   );
 }
 
