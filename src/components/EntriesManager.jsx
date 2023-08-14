3fc435cafdd72481ca844e54e36ed42139eb5ad7
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-14 19:02:22 +0530

    Refactored Carasoul System to make it more managable

--- src/components/EntriesManager.jsx
+++ src/components/EntriesManager.jsx
@@ -1,36 +1,71 @@
 import PropTypes from 'prop-types';
 
-function EntriesManager({index, handleIndexChange, maxLength}) {
-  const navBaseClass='bg-gray-300 border border-gray-600 text-black hover:bg-gray-500 hover:text-white leading-tight py-2 px-3 dark:bg-gray-700 dark:border-white dark:text-white dark:hover:bg-gray-300 dark:hover:text-black cursor-pointer';
-  const incrementIndex = () =>{
-    if(index<maxLength-1){
-      handleIndexChange(index+1);
+const EntriesManager = ({ index, handleIndexChange, maxLength }) => {
+  const baseClass = 'bg-gray-300 border border-gray-600 text-black hover:bg-gray-500 hover:text-white leading-tight py-2 px-3 dark:bg-gray-700 dark:border-white dark:text-white dark:hover:bg-gray-300 dark:hover:text-black cursor-pointer';
+  const activeClass = 'bg-gray-500 border border-gray-600 text-white hover:bg-gray-300 hover:text-gray-900 leading-tight py-2 px-3 dark:bg-gray-300 dark:border-white dark:text-gray-900 dark:hover:bg-gray-50 dark:hover:text-black cursor-pointer';
+
+  const incrementIndex = (e) => {
+    e.preventDefault();
+    if (index < maxLength - 1) {
+      handleIndexChange(index + 1);
     }
   }
-  const decrementIndex = () =>{
-    if(index>0){
-      handleIndexChange(index-1);
+  const decrementIndex = (e) => {
+    e.preventDefault();
+    if (index > 0) {
+      handleIndexChange(index - 1);
     }
   }
-  let navDiv=[];
-  let navItem;
-  for(let i=0;i<maxLength;i++){ 
-    if(i===index){
-      navItem = <a href='#' className='bg-gray-500 border border-gray-600 text-white hover:bg-gray-300 hover:text-gray-900 leading-tight py-2 px-3 dark:bg-gray-300 dark:border-white dark:text-gray-900 dark:hover:bg-gray-50 dark:hover:text-black cursor-pointer' onClick={()=>handleIndexChange(i)}>{i+1}</a>
-    }else{
-      navItem = <a href='#' className={navBaseClass} onClick={()=>handleIndexChange(i)}>{i+1}</a>
+
+  let navDiv = [];
+  for (let i = 0; i < maxLength; i++) {
+    if (i === index) {
+      navDiv.push(
+        <li key={i}>
+          <a 
+            href='#' 
+            className={activeClass} 
+            onClick={(e) => { 
+              e.preventDefault(); 
+              handleIndexChange(i); 
+            }}>{i + 1}
+          </a>
+        </li>
+      );
+    } else {
+      navDiv.push(
+        <li key={i} >
+          <a 
+            href='#' 
+            className={baseClass} 
+            onClick={(e) => { 
+              e.preventDefault(); 
+              handleIndexChange(i); 
+            }}>{i + 1}
+          </a>
+        </li>
+      );
     }
-    navDiv.push(<li key={i} >{navItem}</li>);
   }
   return (
     <nav className='flex justify-center my-2'>
       <ul className='flex flex-row'>
         <li>
-          <a href='#' className={`${navBaseClass} rounded-l-lg `} onClick={decrementIndex}>Previous</a>
+          <a
+            href='#'
+            className={`${baseClass} rounded-l-lg `}
+            onClick={decrementIndex}
+          >Previous
+          </a>
         </li>
         {navDiv}
         <li>
-          <a href='#' className={`${navBaseClass} rounded-r-lg`} onClick={incrementIndex}>Next</a>
+          <a
+            href='#'
+            className={`${baseClass} rounded-r-lg`}
+            onClick={incrementIndex}
+          >Next
+          </a>
         </li>
       </ul>
     </nav>
