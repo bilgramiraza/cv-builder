de9d824f853ecb5d5af2c801606ef2cae96065e6
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-26 04:42:27 +0530

    Fixed Preview Component Scaling

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -8,12 +8,12 @@ function Preview({ personal, education, experience, loadMockData}) {
     return (
       <div key={i}>
         <div className='flex flex-row justify-between font-bold'>
-          <span className='w-4/5 text-2xl'>{edu.eduTitle}</span>
-          <span className='w-1/5 text-xl text-right'>{DateTime.fromISO(edu.endDate).toFormat('MMM yyyy')}</span>
+          <span className='w-4/5 text-[13px]'>{edu.eduTitle}</span>
+          <span className='w-1/5 text-[13px] text-right'>{DateTime.fromISO(edu.endDate).toFormat('MMM yyyy')}</span>
         </div>
-        <div className='flex flex-row justify-between text-xl font-semibold italic'>
-          <span>{edu.desc}</span>
-          <span className='text-right'>{edu.address}</span>
+        <div className='flex flex-row justify-between font-semibold italic'>
+          <span className='text-[12px]'>{edu.desc}</span>
+          <span className='text-[12px] text-right'>{edu.address}</span>
         </div>
       </div>);
   });
@@ -22,50 +22,50 @@ function Preview({ personal, education, experience, loadMockData}) {
     return (
       <div key={i}>
         <div className='flex flex-row justify-between font-bold'>
-          <span className='w-3/5 text-2xl'>{exp.expCompany}</span>
-          <span className='w-2/5 text-xl text-right'>{DateTime.fromISO(exp.startDate).toFormat('MMM yyyy')} - {DateTime.fromISO(exp.endDate).toFormat('MMM yyyy')}</span>
+          <span className='w-3/5 text-[13px] '>{exp.expCompany}</span>
+          <span className='w-2/5 text-[13px] text-right'>{DateTime.fromISO(exp.startDate).toFormat('MMM yyyy')} - {DateTime.fromISO(exp.endDate).toFormat('MMM yyyy')}</span>
         </div>
-        <div className='flex flex-row justify-between text-xl font-semibold italic'>
-          <span>{exp.expTitle}</span>
-          <span className='text-right'>{exp.address}</span>
+        <div className='flex flex-row justify-between font-semibold italic'>
+          <span className='text-[12px]'>{exp.expTitle}</span>
+          <span className='text-[12px] text-right'>{exp.address}</span>
         </div>
-        <div className='flex flex-row text-lg'>
-          <p>{exp.desc}</p>
+        <div className='flex flex-row'>
+          <p className='text-[12px]'>{exp.desc}</p>
         </div>
       </div>);
   });
 
   return (
-    <div className='block w-4/5 mt-6 mx-auto border-4 border-lg rounded-lg bg-white p-2 font-serif text-black'>
+    <div className='block w-[35rem] mt-6 mx-auto border-4 border-lg rounded-lg bg-white p-2 font-serif text-black'>
       <div className='mb-3 flex flex-row justify-between'>
-        <h2 className='font-bold text-6xl'>
-          <span>{personal?.firstName}</span>
-          <span>{' '}</span>
-          <span>{personal?.lastName}</span>
-        </h2>
+        <div className='font-bold'>
+          <span className='text-[24px]'>{personal?.firstName}</span>
+          <span className='text-[24px]'>{' '}</span>
+          <span className='text-[24px]'>{personal?.lastName}</span>
+        </div>
         <div>
           <button className='bg-gray-500 border border-gray-900 rounded-xl text-white font-semibold hover:bg-gray-300 hover:text-gray-900 leading-tight py-2 px-3 cursor-pointer' onClick={loadMockData}>Load Mock Data</button>
         </div>
       </div>
-      <div className='flex flex-row justify-start leading-none w-3/4'>
-        <span className='me-6'>{personal?.email}</span>
-        <span className='me-6'>{personal?.address}</span>
-        <span className='me-6'>{personal?.website}</span>
+      <div className='flex flex-row justify-start flex-grow leading-none w-11/12'>
+        <span className='me-2 text-[12px]'>{personal?.email}</span>
+        <span className='me-2 text-[12px]'>{personal?.address}</span>
+        <span className='text-[12px]'>{personal?.website}</span>
       </div>
       <div>
-        <h3 className='text-3xl font-semibold'>PROFESSIONAL SUMMARY</h3>
+        <p className='text-[14px] font-semibold'>PROFESSIONAL SUMMARY</p>
         <hr className='h-0.5 bg-black border-0 dark:bg-gray-700' />
-        <p className='text-lg'>{personal?.profSummary}</p>
+        <p className='text-[12px] '>{personal?.profSummary}</p>
       </div>
       <div>
-        <h3 className='text-3xl font-semibold'>PROFESSIONAL EXPERIENCE</h3>
+        <p className='text-[14px] font-semibold'>PROFESSIONAL EXPERIENCE</p>
         <hr className='h-0.5 bg-black border-0 dark:bg-gray-700' />
         <div>
           {experienceDiv}
         </div>
       </div>
       <div>
-        <h3 className='text-3xl font-semibold'>EDUCATION</h3>
+        <p className='text-[14px] font-semibold'>EDUCATION</p>
         <hr className='h-0.5 bg-black border-0 dark:bg-gray-700' />
         <div>
           {educationDiv}
